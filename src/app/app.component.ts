cb008dadaf415286cbf4636df1b6c40327b3b912
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-02-19 20:19:42 +0200

    I have created a graph.component and added it to app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GraphComponent } from './components/graph/graph.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SieGraph';
+
+
 }
---
+++ src/app/components/graph/graph.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  Chart,
+  ChartType,
+  ChartOptions,
+  ChartData,
+  Colors,
+  BubbleController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  registerables,
+} from 'node_modules/chart.js';
+
+
+Chart.register(...registerables);
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss'],
+})
+
+export class GraphComponent {
+
+  constructor() {
+  }
+
+  ngOnInit(){
+    this.ShowGraph();
+  }
+
+  public lineChartType: ChartType = "line";
+
+  // Input information. Send it to html to id of canvas.
+  tagName: string = "tagNameId";
+
+  ShowGraph(){
+    new Chart(this.tagName, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: ' Value ',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 3
+        }]
+      },
+      options: {
+        //plugins: {
+        //  legend: false,
+        //  tooltip: true,
+        //},
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
